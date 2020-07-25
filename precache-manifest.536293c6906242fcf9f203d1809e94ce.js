4b0cff8f45152a2bab82cc81325cb27262511f8e
pomubry <btaduran30@protonmail.com>
2020-07-25 11:15:19 +0800

    Updates

---
+++ precache-manifest.536293c6906242fcf9f203d1809e94ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "103dadc413bcafb86209423f82711d39",
+    "url": "/jscalculator/index.html"
+  },
+  {
+    "revision": "c1e298d59289f370528f",
+    "url": "/jscalculator/static/css/main.6c4ab534.chunk.css"
+  },
+  {
+    "revision": "fcb255844dde62e96c70",
+    "url": "/jscalculator/static/js/2.04f6746e.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/jscalculator/static/js/2.04f6746e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1e298d59289f370528f",
+    "url": "/jscalculator/static/js/main.b99c42fd.chunk.js"
+  },
+  {
+    "revision": "460c06652e9e993c4d1f",
+    "url": "/jscalculator/static/js/runtime-main.8506a97e.js"
+  }
+]);
